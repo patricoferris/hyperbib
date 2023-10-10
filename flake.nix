d81786693432145e99c0c0a012a82a6cfcbe40fd
Ryan Gibb <ryan@freumh.org>
2023-10-10 20:13:03 +0100

    module

--- flake.nix
+++ flake.nix
@@ -11,24 +11,132 @@
     opam-nix.inputs.opam-repository.follows = "opam-repository";
   };
   outputs = { self, flake-utils, opam-nix, nixpkgs, ... }@inputs:
-    let package = "hyperbib";
-    in flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
+        package = "hyperbib";
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
-        scope =
-          on.buildOpamProject' { } ./. {
-            ocaml-base-compiler = "*";
-            # dune = "*";
-            # ptime = "*";
-            };
-        overlay = final: prev:
-          {
-            # Your overrides go here
-          };
+        scope = on.buildOpamProject' { } ./. { ocaml-base-compiler = "*"; };
       in {
-        legacyPackages = scope.overrideScope' overlay;
-
+        legacyPackages = scope;
         packages.default = self.legacyPackages.${system}.${package};
-      });
+      }
+    ) // {
+      nixosModules.default = ({ pkgs, config, lib, ... }:
+        with lib;
+
+        let cfg = config.services.hyperbib; in
+        {
+          options = {
+            services.hyperbib = {
+              enable = mkEnableOption "hyperbib website";
+              domain = mkOption {
+                type = types.str;
+                default = "example.org";
+              };
+              port = lib.mkOption {
+                type = lib.types.port;
+                default = 8080;
+              };
+              user = lib.mkOption {
+                type = lib.types.str;
+                default = "hyperbib";
+              };
+              group = lib.mkOption {
+                type = lib.types.str;
+                default = cfg.user;
+              };
+              servicePath = lib.mkOption {
+                type = lib.types.string;
+                default = "/mybibliography";
+              };
+              appAir = lib.mkOption {
+                type = lib.types.string;
+                default = "/var/www/hyperbib";
+              };
+            };
+          };
+
+          config = mkIf cfg.enable {
+            services.nginx = {
+              enable = true;
+              virtualHosts = {
+                "${cfg.domain}" = {
+                  forceSSL = true;
+                  enableACME = true;
+                  locations."${cfg.servicePath}" = {
+                    proxyPass = "http://127.0.0.1:${builtins.toString cfg.port}/";
+                  };
+                };
+              };
+            };
+
+            systemd.services.hyperbib = {
+              enable = true;
+              description = "hyperbib";
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                ExecStart =
+                  "${self.packages.${config.nixpkgs.hostPlatform.system}.default}/bin/hyperbib" +
+                  " serve" +
+                  " --listen localhost:${builtins.toString cfg.port}" +
+                  " --service-path ${cfg.servicePath}" +
+                  " --app-dir ${cfg.appAir}";
+                Restart = "on-failure";
+                RestartSec = "10s";
+                User = cfg.user;
+                Group = cfg.group;
+              };
+            };
+
+            users.users = {
+              "${cfg.user}" = {
+                description = "hyperbib service";
+                useDefaultShell = true;
+                group = cfg.group;
+                isSystemUser = true;
+              };
+            };
+
+            users.groups."${cfg.group}" = {};
+          };
+        });
+
+      nixosConfigurations."container" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ pkgs, ... }: {
+            boot.isContainer = true;
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+            networking.useDHCP = false;
+            networking.firewall.allowedTCPPorts = [ 80 ];
+            services.nginx = {
+              enable = true;
+              virtualHosts."_" = {
+                locations."/" = {
+                   proxyPass = "http://127.0.0.1:8080";
+                 };
+              };
+            };
+            systemd.services.hyperbib = {
+              enable = true;
+              description = "hyperbib";
+              serviceConfig = {
+                ExecStart = ''
+                  ${self.packages."x86_64-linux".default}/bin/hyperbib
+                  --listen localhost:8000
+                  --service-path /mybibliography/
+                  --app-dir /var/www/hyperbib
+                '';
+              };
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+              environment.PORT = "8080";
+            };
+            system.stateVersion = "22.11";
+          })
+        ];
+      };
+    };
 }
